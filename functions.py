3e146d8f095da0bfc264cad68f1aa5d27002ce3a
hydroptix <samuelfrazee@hotmail.com>
2020-03-30 17:11:48 -0700

    Modified individual player reporting to use f-strings

--- functions.py
+++ functions.py
@@ -15,11 +15,9 @@ def getPlayerStatsString(player_name: str) -> str:
         ret_str += "\n\nPlease try again with one of the above names"
 
     else:
-        ret_str = "Player " + str(players_info[0].get('id')) + ": " + \
-                  players_info[0].get('full_name') + \
-                  "\n\t"
+        ret_str = f"Player {str(players_info[0].get('id'))}: {players_info[0].get('full_name')}"
 
-        ret_str += "Status: "
+        ret_str += "\n\tStatus: "
         if players_info[0].get('is_active'):
             ret_str += "Active"
         else:
@@ -27,15 +25,15 @@ def getPlayerStatsString(player_name: str) -> str:
 
         # get the rest of the data from the NBA api endpoint
         # might want to change this to DataFrame if we need it for graphing later
-        endpoint_info = commonplayerinfo.CommonPlayerInfo(player_id=players_info[0].get('id')) \
+        ept_info = commonplayerinfo.CommonPlayerInfo(player_id=players_info[0].get('id')) \
             .get_normalized_dict().get('CommonPlayerInfo')[0]
 
-        ret_str += "\n\t#" + endpoint_info.get("JERSEY") + " on the " + endpoint_info.get('TEAM_CITY') + " " + \
-                   endpoint_info.get('TEAM_NAME')
+        ret_str += f" ({ept_info.get('FROM_YEAR')}-{ept_info.get('TO_YEAR')})"
+
+        ret_str += f"\n\t{ept_info.get('TEAM_CITY')} {ept_info.get('TEAM_NAME')} #{ept_info.get('JERSEY')}: {ept_info.get('POSITION')}"
 
         #split the height into separate feet and inches for formatting
-        height = endpoint_info.get("HEIGHT").split('-')
-        ret_str += "\n\tHeight: " + height[0] + "\'" + height[1] + "\", Weight: " + endpoint_info.get("WEIGHT") + " lbs"
-
+        height = ept_info.get('HEIGHT').split('-')
+        ret_str += f"\n\tHeight: {height[0]}\'{height[1]}\", Weight: {ept_info.get('WEIGHT')} lbs"
 
     return ret_str
