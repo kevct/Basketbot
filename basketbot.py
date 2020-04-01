cbf62c3a373e483b4790c323ad52dd35c8042928
Kevin <55123553+kevct@users.noreply.github.com>
2020-04-01 00:37:33 -0700

    added functionality to the bot

--- basketbot.py
+++ basketbot.py
@@ -6,29 +6,56 @@ Created on Sat Mar 30 2020
 import discord
 import os
 import discord.ext.commands as commands
-import random
-from regex_parsing import parse_discord_message
+import functions as nba
 
-TOKEN = ""
+TOKEN = open("token.txt", 'r').read()
 
-bot = commands.Bot(command_prefix='-')
+bot = commands.Bot(command_prefix='%')
 
 @bot.command()
-async def command(ctx):
-    """Put what command does here"""
-
+async def playercareerstats(ctx, *args):
+    """[full or partial name] Displays a player's career statistics"""
+    name = ""
+    for x in args:
+        name = name + " " + x
+    
+    embed = discord.Embed(title="test", description="test", color = 0x595959)
+    players = nba.getPlayerIdsByName(name.strip())
+    if not players == None:
+        if len(players) > 1:
+            embed = discord.Embed(title="Multiple Players", description="There are multiple players with the name \"" + name.strip() + "\":", color = 0x595959)
+            for i in players:
+                embed.add_field(name="\u200b", value=i[1], inline=True)
+        else:
+            stats = nba.getPlayerCareerStatsByID(players[0][0])
+            height = stats["HEIGHT"].split("-")
+            embed = discord.Embed(title=players[0][1], description= stats["TEAM_CITY"] + " " + stats["TEAM_NAME"] + " | #" + stats["JERSEY"] + " | "  
+                + stats["POSITION"] + "\n" + height[0] + "'" + height[1] + "\"" + " | " + stats["WEIGHT"] + " lbs" + " | " + str(stats["FROM_YEAR"]) + " - " + str(stats["TO_YEAR"]), color = nba.getTeamColor(stats["TEAM_ID"]))
+            embed.set_thumbnail(url=nba.getPlayerHeadshotURL(players[0][0]))
+            embed.add_field(name="Career Points", value=stats["PTS"], inline=False)
+            embed.add_field(name="Career Rebounds", value=stats["REB"], inline=False)
+            embed.add_field(name="Career Assists", value=stats["AST"], inline=False)
+            embed.add_field(name="Career Blocks", value=stats["BLK"], inline=False)
+            embed.add_field(name="Career Steals", value=stats["STL"], inline=False)
+    else:
+        embed = discord.Embed(title="Player Not Found", description="Double check that the player name is spelled correctly.", color = 0x595959)
+    await ctx.send(embed=embed)
+    
 @bot.event
 async def on_message(message):
     """Do something when a message is sent"""
+    if not message.author == bot.user:
+        pass
+    await bot.process_commands(message)
 
 @bot.event
 async def on_command_error(ctx, error):
     print(error)
-    await ctx.send("Invalid command. See -help for commands.")
+    embed = discord.Embed(title="Invalid Command", description="See %help for commands.", color = 0x595959)
+    await ctx.send(embed=embed)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(""))
     print("Logged in as")
     print(bot.user.name)
     print(bot.user.id)
---
+++ functions.py
@@ -0,0 +1,252 @@
+from typing import Optional, Union, List, Dict, Any
+
+from nba_api.stats.static import players, teams
+from nba_api.stats.endpoints import PlayerCareerStats, TeamInfoCommon
+from nba_api.stats.endpoints.commonplayerinfo import CommonPlayerInfo
+from nba_api.stats.endpoints.teamyearbyyearstats import TeamYearByYearStats
+from nba_api.stats.library.parameters import Season
+from nba_api.stats.library.data import teams as teams
+
+#This is a constant that I don't really expect to change
+DEFAULT_DISPLAY_LENGTH = 10
+
+teamClrs = {
+    1610612737: 0xE03A3E, #Atlanta Hawks
+    1610612738: 0x007A33, #Boston Celtics
+    1610612739: 0x860038, #Cleveland Cavaliers
+    1610612740: 0x0C2340, #New Orleans Pelicans
+    1610612741: 0xCE1141, #Chicago Bulls
+    1610612742: 0x00538C, #Dallas Mavericks
+    1610612743: 0x0E2240, #Denver Nuggets
+    1610612744: 0x006BB6, #Golden State Warriors
+    1610612745: 0xCE1141, #Houston Rockets
+    1610612746: 0x1D428A, #Los Angeles Clippers
+    1610612747: 0xFDB927, #Los Angeles Lakers
+    1610612748: 0x98002E, #Miami Heat
+    1610612749: 0x00471B, #Milawukee Bucks
+    1610612750: 0x0C2340, #Minnesota Timberwolves
+    1610612751: 0x000000, #Brooklyn Nets
+    1610612752: 0xF58426, #New York Knicks
+    1610612753: 0x0077C0, #Orlando Magic
+    1610612754: 0xFDBB30, #Indiana Pacers
+    1610612755: 0x006BB6, #Philidelphia 76ers
+    1610612756: 0xE56020, #Phoenix Suns
+    1610612757: 0xE03A3E, #Portland Trail Blazers
+    1610612758: 0x5A2D81, #Sacramento Kings
+    1610612759: 0xC4CED4, #San Antonio Spurs
+    1610612760: 0x007AC1, #Oklahoma City Thunder
+    1610612761: 0xCE1141, #Toronto Raptors
+    1610612762: 0xF9A01B, #Utah Jazz
+    1610612763: 0x5D76A9, #Memphis Grizzlies
+    1610612764: 0x002B5C, #Washington Wizards
+    1610612765: 0xC8102E, #Detroit Pistons
+    1610612766: 0x00788C  #Charlotte Hornets
+}
+
+def getPlayerSeasonStatsByID(player_id: int, season_id: str = Season.current_season) -> Optional[dict]:
+    static_info = players.find_player_by_id(player_id)
+
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    all_seasons = PlayerCareerStats(player_id=static_info.get('id')).get_normalized_dict().get('SeasonTotalsRegularSeason')
+
+    target_season = None
+
+    for season in all_seasons:
+        if season.get('SEASON_ID') == season_id:
+            target_season = season
+            break
+
+    if target_season is None:
+        return None
+
+    else:
+        stats_dict = {}
+
+        stats_dict['PTS'] = target_season.get('PTS')
+        stats_dict['AST'] = target_season.get('AST')
+        stats_dict['BLK'] = target_season.get('BLK')
+        stats_dict['STL'] = target_season.get('STL')
+        stats_dict['REB'] = target_season.get('REB')
+        stats_dict['OREB'] = target_season.get('OREB')
+        stats_dict['DREB'] = target_season.get('DREB')
+
+        return stats_dict
+
+def getPlayerCareerStatsByID(player_id: int) -> Optional[dict]:
+    static_info = players.find_player_by_id(player_id)
+
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    stats_dict = {}
+
+    common_info = CommonPlayerInfo(player_id=static_info.get('id')).get_normalized_dict() \
+        .get('CommonPlayerInfo')[0]
+    career_stats = PlayerCareerStats(player_id=static_info.get('id')).get_normalized_dict() \
+        .get('CareerTotalsRegularSeason')[0]
+
+    stats_dict['FROM_YEAR'] = common_info.get('FROM_YEAR')
+    stats_dict['TO_YEAR'] = common_info.get('TO_YEAR')
+    stats_dict['TEAM_ID'] = common_info.get('TEAM_ID')
+    stats_dict['TEAM_CITY'] = common_info.get('TEAM_CITY')
+    stats_dict['TEAM_NAME'] = common_info.get('TEAM_NAME')
+    stats_dict['JERSEY'] = common_info.get('JERSEY')
+    stats_dict['POSITION'] = common_info.get('POSITION')
+    stats_dict['HEIGHT'] = common_info.get('HEIGHT')
+    stats_dict['WEIGHT'] = common_info.get('WEIGHT')
+    stats_dict['PTS'] = career_stats.get('PTS')
+    stats_dict['AST'] = career_stats.get('AST')
+    stats_dict['BLK'] = career_stats.get('BLK')
+    stats_dict['STL'] = career_stats.get('STL')
+    stats_dict['REB'] = career_stats.get('REB')
+    stats_dict['OREB'] = career_stats.get('OREB')
+    stats_dict['DREB'] = career_stats.get('DREB')
+
+    return stats_dict
+
+def getPlayerCareerString(player_id: int) -> Optional[str]:
+    static_info = players.find_player_by_id(player_id)
+
+    #If that id doesn't return a player, return None
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    ret_str = f"**Player {str(static_info.get('id'))}: {static_info.get('full_name')}**"
+
+    ret_str += "\n\tStatus: "
+    if static_info.get('is_active'):
+        ret_str += "Active"
+    else:
+        ret_str += "Inactive"
+
+    # get the rest of the data from the NBA api endpoint
+    # might want to change this to DataFrame if we need it for graphing later
+    all_info = getPlayerCareerStatsByID(player_id)
+
+    ret_str += f" ({all_info.get('FROM_YEAR')}-{all_info.get('TO_YEAR')})"
+
+    ret_str += f"\n\t{all_info.get('TEAM_CITY')} {all_info.get('TEAM_NAME')} #{all_info.get('JERSEY')}: {all_info.get('POSITION')}"
+
+    #split the height into separate feet and inches for formatting
+    height = all_info.get('HEIGHT').split('-')
+    ret_str += f"\n\tHeight: {height[0]}\'{height[1]}\", Weight: {all_info.get('WEIGHT')} lbs"
+
+    #career stats
+    ret_str += "\n\n\t*Career Stats (Regular Season):*"
+    ret_str += f"\n\tPoints: {all_info.get('PTS')}"
+    ret_str += f"\n\tAssists: {all_info.get('AST')}"
+    ret_str += f"\n\tBlocks: {all_info.get('BLK')}"
+    ret_str += f"\n\tSteals: {all_info.get('STL')}"
+    ret_str += f"\n\tRebounds: {all_info.get('REB')}"
+    ret_str += f"\n\t   Offensive: {all_info.get('OREB')}"
+    ret_str += f"\n\t   Defensive: {all_info.get('DREB')}"
+
+    return ret_str
+
+def getPlayerIdsByName(player_name: str) -> Optional[List[List]]:
+    """
+    Takes a string and returns all the names and IDs matching the string
+    :param player_name: name of the player to search for
+    :return: returns None if no matches found, returns an array of matching ids and their names otherwise
+    """
+
+    all_matches = players.find_players_by_full_name(player_name)
+    ret_list = []
+
+    if len(all_matches) < 1:
+        return None
+    else:
+        for match in all_matches:
+            ret_list.append([match.get('id'), match.get('full_name')])
+
+    return ret_list
+
+def getPlayerHeadshotURL(player_id: int) -> Optional[str]:
+    static_info = players.find_player_by_id(player_id)
+
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    return f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{str(player_id)}.png"
+
+def getTeamCareerStatsByID(team_id: int) -> Optional[str]:
+    static_info = teams.find_team_name_by_id(team_id)
+
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    stats_dict = {}
+
+    all_seasons = TeamYearByYearStats(team_id = team_id).get_normalized_dict().get('TeamStats')
+    stats_dict['W'] = 0
+    stats_dict['L'] = 0
+    stats_dict['PCT'] = 0
+    stats_dict['MAX_CONF_RANK'] = all_seasons[0].get('CONF_RANK')
+    stats_dict['MIN_CONF_RANK'] = all_seasons[0].get('CONF_RANK')
+    stats_dict['MAX_DIV_RANK'] = all_seasons[0].get('DIV_RANK')
+    stats_dict['MIN_DIV_RANK'] = all_seasons[0].get('DIV_RANK')
+    # Add up all the stats for all seasons
+    for season in all_seasons:
+        stats_dict['W'] += season.get('WINS')
+        stats_dict['L'] += season.get('LOSSES')
+
+        if stats_dict['MAX_CONF_RANK'] > season.get('CONF_RANK') or stats_dict['MAX_CONF_RANK'] < 1:
+            stats_dict['MAX_CONF_RANK'] = season.get('CONF_RANK')
+
+        elif stats_dict['MIN_CONF_RANK'] < season.get('CONF_RANK'):
+            stats_dict['MIN_CONF_RANK'] = season.get('CONF_RANK')
+
+        if stats_dict['MAX_DIV_RANK'] > season.get('DIV_RANK'):
+            stats_dict['MAX_DIV_RANK'] = season.get('DIV_RANK')
+
+        elif stats_dict['MIN_DIV_RANK'] < season.get('DIV_RANK'):
+            stats_dict['MIN_DIV_RANK'] = season.get('DIV_RANK')
+
+    stats_dict['PCT'] = (stats_dict['W'] + stats_dict['L']) / float(stats_dict['W'])
+
+    return stats_dict
+
+def getTeamSeasonStatsByID(team_id: int, season_id: str = Season.current_season) -> Optional[Dict[str, Any]]:
+    static_info = teams.find_team_name_by_id(team_id)
+
+    if static_info is None or len(static_info) < 1:
+        return None
+
+    stats_dict = {}
+
+    season_info = TeamInfoCommon(team_id = team_id, season_nullable=season_id).get_normalized_dict().get('TeamInfoCommon')[0]
+
+    stats_dict['TEAM_CONFERENCE'] = season_info.get('TEAM_CONFERENCE')
+    stats_dict['CONF_RANK'] = season_info.get('CONF_RANK')
+    stats_dict['TEAM_DIVISION'] = season_info.get('TEAM_DIVISION')
+    stats_dict['DIV_RANK'] = season_info.get('DIV_RANK')
+    stats_dict['W'] = season_info.get('W')
+    stats_dict['L'] = season_info.get('L')
+    stats_dict['PCT'] = season_info.get('PCT')
+
+    return stats_dict
+
+def getTeamIdsByName(team_name: str) -> Optional[List[List]]:
+    """
+    Takes a string name and returns a list of all the teams and ids that match that string
+    :param team_name:
+    :return:
+    """
+    all_matches = teams.find_teams_by_full_name(team_name)
+    ret_list = []
+
+    if len(all_matches) < 1:
+        return None
+
+    else:
+        for match in all_matches:
+            ret_list.append([match.get('id'), match.get('full_name')])
+
+    return ret_list
+    
+def getTeamColor(team_code: int) -> int:
+    if team_code in teamClrs:
+        return teamClrs[team_code]
+    return 0xFFFFFE
