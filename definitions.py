03e78265a041706fee5d3cd5015e3be7e5623e07
hydroptix <samuelfrazee@hotmail.com>
2020-06-24 23:32:55 -0700

    Added folder creation for needed folders that don't exist

--- definitions.py
+++ definitions.py
@@ -1,4 +1,5 @@
-from os.path import dirname, abspath, join
+from os.path import dirname, abspath, join, exists
+from os import makedirs
 
 # Root backend directory
 # join used here to eliminate problems with different filesystem formats
@@ -10,3 +11,10 @@ TOKEN_FILE = join(CONF_PATH, "token.txt")
 GOOD_PROXIES_FILE = join(CONF_PATH, 'good_proxies.txt')
 BAD_PROXIES_FILE = join(CONF_PATH, 'bad_proxies.txt')
 BLOCKED_PROXIES_FILE = join(CONF_PATH, 'blocked_proxies.txt')
+
+# Ensure the conf and log folders exist
+if not exists(CONF_PATH):
+    makedirs(CONF_PATH)
+
+if not exists(LOG_PATH):
+    makedirs(LOG_PATH)
